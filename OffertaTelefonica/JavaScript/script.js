c8396c5cadb8f7d80d6267bffe6eab40541fdada
chripiana <pianarosachristian@gmail.com>
2021-04-22 19:59:58 +0200

    Update script.js

--- OffertaTelefonica/JavaScript/script.js
+++ OffertaTelefonica/JavaScript/script.js
@@ -10,21 +10,21 @@ function inizializzaSelection()
         document.getElementById("sesso").appendChild(s);
     }
     //inserimento opzioni occhi
-    var coloreOcchi=["Nero","Castano","Verde","Blu","Ambra"];
+    var occhi=["Nero","Castano","Verde","Blu","Ambra"];
     for(var i=0;i<5;i++)
     {
         var o=document.createElement("option");
-        o.value=coloreOcchi[i];
-        o.innerHTML=coloreOcchi[i];
+        o.value=occhi[i];
+        o.innerHTML=occhi[i];
         document.getElementById("occhi").appendChild(o);
     }
     //inserimento opzioni provincia
-    var provincia=["BG","BS","CO","CR","LC","LO","MN","MI","MB","PV","SO","VA"];
+    var residenza=["BG","BS","CO","CR","LC","LO","MN","MI","MB","PV","SO","VA"];
     for(var i=0;i<12;i++)
     {
         var p=document.createElement("option");
-        p.value=provincia[i];
-        p.innerHTML=provincia[i];
+        p.value=residenza[i];
+        p.innerHTML=residenza[i];
         document.getElementById("residenza").appendChild(p);
     }
 }
